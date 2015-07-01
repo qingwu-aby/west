3036cdf00265271d5f808362a304f625fdb8f9df
Yin-Far <511865614@qq.com>
2015-07-01 22:51:47 +0800

    add

---
+++ episode.php
@@ -0,0 +1,89 @@
+<?php include_once './view/header.html'; ?>
+<div id="content" class="content">
+	<div class="war">
+		<div class="content-nav">
+			
+		</div>
+
+		<div class="content-main">
+			
+			<div class="yMain-left row">
+				<div>
+					<span class="episode"></span>
+					<span class="text">最新剧集 <i class="eng">UPDATE EPISODE</i></span>
+					<hr />
+				</div>
+				
+				
+				<ul class="list"id="list">					
+				</ul>
+				<!-- <div class="get_more">
+				    <a href="javascript:void(0);" id="get_more" style="color:#333;">>>查看更多<<</a>
+				</div> -->		
+			</div>
+			<div class="yMain-right row">
+				<ul>
+					<li class="company_row">
+					<div>
+						<span class="company"></span>
+						<span class="text" id="company_text">公司简介</span><i class="eng"> COMPANY PROFILE</i>
+						<hr/>
+					</div>
+					<div id="summary_company">					
+						<p class="summary">
+						 	
+						</p>
+					</div>
+					</li>
+					<li>
+						<div>
+							<span class="film"></span>
+							<span class="text">影视作品</span><i class="eng"> FILM AND TELEVISION</i>
+							<hr/>
+						</div>
+						<div class="thumbnail">
+							<ul id="thum_list">
+								
+							</ul>
+						</div>
+					</li>
+					<li>
+						<div>
+							<span class="contact"></span>
+							<span class="text">联系方式</span><i class="eng"> CONTACT WAY</i>
+							<hr/>
+						</div>
+						<div class="linkme">
+							<p>联系方式： <span id="mail"></span></p>
+							<p>联系电话： <span id="tel"></span></p>
+							<div id="wechat">
+							<!-- <span id="linkme_images"><img src="./images/qrcode.png" /></span> -->
+							</div>
+						</div>	
+					</li>				
+				</ul>	
+			</div>
+		</div>
+	</div>
+</div>
+	<script type="text/javascript" src="./js/jquery-1.9.1.min.js"></script>
+	<script type="text/javascript" src="./js/module.js"></script>
+	<script type="text/javascript" src="./js/company.js"></script>
+	<script type="text/javascript" src="./js/video.js"></script>
+	<script type="text/javascript" src="./js/getepisode.js"></script>
+	<script type="text/javascript">
+		$(document).ready(function(){
+			$('.nav-list').removeClass('on');
+			$('#nav-list-4').addClass('on');
+			$('title').text('剧集-陕西省影视产业管理平台系统');
+			//限制可显示字数为83个汉字
+			$(".article").each(function(){
+		        var maxwidth=83;
+		        if($(this).text().length>maxwidth){
+		            $(this).text($(this).text().substring(0,maxwidth));
+		            $(this).html($(this).html()+'...');
+		        }
+		    });
+		});
+	</script>
+<?php include_once './view/footer.html'; ?>
---
+++ js/getepisode.js
@@ -0,0 +1,52 @@
+$(document).ready(function(){
+var i = 0;  
+function getYmdTime(time){
+		if(time > 0){
+			var dateStr = new Date(time);
+			return dateStr.getFullYear() + '年' + (dateStr.getMonth()+1) +'月' + dateStr.getDate() + '日';
+		}else{
+			return '未知时间';
+		}
+	} 
+    function getData() { 
+            i++; 
+                var link=window.location.search;
+                var index=link.split('?')[1];
+                var oUid = index.substr(4);
+                 $.ajax({
+                    type:'get',
+                    async:false,
+                    url:"server/index.php?m=Home&c=index&a=getNewsList",
+                    data:{uid:oUid,p:i}, 
+                    dataType:'json',
+                    success:function(data){  
+                            if(data){  
+                                var str = "";  
+                                $.each(data.data,function(index,array){  
+                                    str = '<li class="list_img"><div class="img">';  
+                                    str = str + '<a href="./detail.php?uid='+array['uid']+'&id='+array['id']+'" target="_blank"><span class="list_img_pic"><img src="'+array['thumbnail'].substr(1)+'"/></span></a>';  
+                                    str = str + '</div><div class="info"><a href="./detail.php?uid='+array['uid']+'&id='+array['id']+'"><h4 class="header_info">'+array['title']+'</h4></a>';  
+                                    str = str + '<p class="article">'+array['summary']+'</p>';  
+                                    var date_str = getYmdTime(new Date(array['modify_time'] * 1000));
+                                    str = str + '<i class="date">'+date_str+'</i></div>';
+                                    $("#list").append(str);  
+                                })
+                                  
+                            }else{  
+                                $(".nodata").show().alert("别点击了，已经到底了。。。");  
+                                return false;  
+                            }  
+                        } 
+                    }); 
+                    
+                }  
+            getData(i);
+    
+            //继续加载按钮事件  
+            $("#get_more").click(function () {  
+                getData(i);  
+                $(window).scroll(scrollHandler);  
+            });
+            
+         
+    });
