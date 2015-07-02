d7f669ec15ed5638a217136b51e7ab0a68148392
我才是二亮 <lwl@xuptsec.org>
2015-07-02 20:34:22 +0800

    添加剧集模块

--- server/Apps/Home/Controller/ViewsController.class.php
+++ server/Apps/Home/Controller/ViewsController.class.php
@@ -140,4 +140,59 @@ class ViewsController extends Controller {
 		$this->ajaxReturn($data);
 		return ;
 	}
+
+	/**
+	 * 获取剧集列表
+	 */
+	public function getEpisodesList() {
+
+		$limit = 10000;
+		$p = I('get.p');
+		//if(IS_POST) {
+			$uid = I('get.uid');
+			$Episodes = M('Episodes'); // 实例化User对象
+			$list = $Episodes->where(array('status' => 1))->where(array('uid' => $uid))->order('id DESC')->limit($limit)->page($p)->select();
+			$count = $Episodes->count();// 查询满足要求的总记录数
+			$data = array(
+				'code' => 1,
+				'count' => $count,
+				'data' => $list,
+			);
+		// } else {
+		// 	$data = array(
+		// 		'code' => -1,
+		// 		'msg' => '操作失败',
+		// 	);
+		// }
+		
+		$this->ajaxReturn($data);
+		return;
+	}
+
+	/**
+	 * 获取单条剧集内容
+	 */
+	public function getEpisodesContent() {
+		
+		$uid = I('get.uid');
+		$id = I('get.id');
+		$Episodes = M('Episodes');
+		$result = $Episodes->where(array('uid' => $uid, 'id' => $id))->find();
+		if($result == null) {
+			$data = array(
+				'code' => -1,
+				'msg' => '该数据不存在',
+			);
+		} else {
+			// 增加访问量
+			$update = $Episodes->where(array('uid' => $uid, 'id' => $id))->data(array('views' => $result['views'] + 1))->save();
+
+			$data = array(
+				'code' => 1,
+				'data' => $result,
+			);
+		}
+		$this->ajaxReturn($data);
+		return;
+	}
 }
---
+++ server/Apps/User/Controller/EpisodesController.class.php
@@ -0,0 +1,176 @@
+<?php
+// +----------------------------------------------------------------------
+// | 用户管理模型
+// +----------------------------------------------------------------------
+// | Copyright (c) 2014  All rights reserved.
+// +----------------------------------------------------------------------
+// | Author: 我才是二亮 <unstring@163.com>
+// +----------------------------------------------------------------------
+// | Time: 2015-05-06
+// +----------------------------------------------------------------------
+namespace User\Controller;
+use Think\Controller;
+class EpisodesController extends BaseController {
+    
+    /**
+     * 增加新闻
+     */
+    public function add() {
+
+    	if($this->isLogin()) {
+    		if(IS_POST) {
+    			$uid = session('uid');
+    			// 获取POST数据
+    			$data = array(
+    				'uid' => $uid,
+    				'title' => I('post.title'),
+    				'big_img' => 'no',
+    				'thumbnail' => I('post.thumbnail'),
+    				'summary' => I('post.summary'),
+                    'source' => I('post.source'),
+                    'author' => I('post.author'),
+    				'content' => I('post.content'),
+    				'release_time' => time(),
+                    'status' => 1,
+                    'recommend' => 0,
+                    'top' => 0,
+    				'modify_time' => time(),
+    			);
+
+    			$Episodes = M('Episodes');
+    			$result = $Episodes->data($data)->add();
+    			if($result) {
+					_log($uid, 'add', 'episodes', $result, $data['title']);
+    				$data = array(
+    					'code' => 1,
+    					'msg' => '剧集添加成功',
+    				);
+    			} else {
+    				$data = array(
+    					'code' => -1,
+    					'msg' => '剧集添加失败，请重试',
+    				);
+    			}
+    		} else {
+    			$data = array(
+    				'code' => 0,
+    				'msg' => '操作失败',
+    			);
+    		}
+    	} else {
+    		$data = array(
+    			'code' => -4011,
+    			'msg' => '请登录后操作',
+    		);
+    	}
+    	$this->ajaxReturn($data);
+    	return;
+    }
+
+    /**
+     * 编辑新闻
+     */
+    public function edit() {
+
+    	if($this->isLogin()) {
+    		if(IS_POST) {
+    			$id = I('post.id');
+    			$uid = session('uid');
+    			$data = array(
+    				'title' => I('post.title'),
+    				'big_img' => 'no',
+    				'thumbnail' => I('post.thumbnail'),
+    				'summary' => I('post.summary'),
+                    'source' => I('post.source'),
+                    'author' => I('post.author'),
+    				'content' => I('post.content'),
+    				'modify_time' => time(),
+    			);
+    			$Episodes = M('Episodes');
+    			$result = $Episodes->where("id = $id")->find();
+    			if($result != null && $result['uid'] == $uid) {
+    				$result = $Episodes->where("id = $id")->data($data)->save();
+    				if($result != false) {
+    					_log($uid, 'edit', 'episodes', $id, $data['title']);
+    					$data = array(
+    						'code' => 1,
+    						'msg' => '更新成功',
+    					);
+    				} else {
+    					$data = array(
+    						'code' => -2,
+    						'msg' => '更新失败，请重试',
+    					);
+    				}
+    			} else {
+    				$data = array(
+    					'code' => -1,
+    					'msg' => 'ID对应失败，请重试',
+    				);
+    			}
+    		} else {
+    			$data = array(
+    				'code' => 0,
+    				'msg' => '操作失败',
+    			);
+    		}
+    	} else {
+    		$data = array(
+    			'code' => -4011,
+    			'msg' => '请登录后操作',
+    		);
+    	}
+
+    	$this->ajaxReturn($data);
+    	return;
+    }
+
+    /**
+     * 删除新闻
+     */
+    public function del() {
+
+    	if($this->isLogin()) {
+    		if(IS_POST) {
+    			$uid = session('uid');
+    			$id = I('post.id');
+    			$Episodes = M('Episodes');
+    			$result = $Episodes->where("id = $id")->find();
+    			if($result != null && $result['uid'] == $uid) {
+                    $title = $result['title'];
+    				//$result = $Episodes->where("id = $id")->delete();
+    				$result = $Episodes->where("id = $id")->data(array('status' => 0))->save();
+                    if($result != false) {
+    					_log($uid, 'del', 'episodes', $id, $title);
+    					$data = array(
+    						'code' => 1,
+    						'msg' => '删除成功',
+    					);
+    				} else {
+    					$data = array(
+    						'code' => -2,
+    						'msg' => '删除失败',
+    					);
+    				}
+    			} else {
+    				$data = array(
+    					'code' => -1,
+    					'msg' => 'ID对应失败，请重试',
+    				);
+    			}
+    		} else {
+    			$data = array(
+    				'code' => 0,
+    				'msg' => '操作失败',
+    			);
+    		}
+    	} else {
+    		$data = array(
+    			'code' => -4011,
+    			'msg' => '请登录后操作',
+    		);
+    	}
+    	$this->ajaxReturn($data);
+    	return;
+    }
+}
