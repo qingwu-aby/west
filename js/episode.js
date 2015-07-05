4a99dcc65a6253a4c8568b3738228455b2d5fc07
Yin-Far <511865614@qq.com>
2015-07-05 09:15:40 +0800

    update

--- js/episode.js
+++ js/episode.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
       var oUid = index.substr(4);
       $.ajax({
             type:'get',
-            url:"server/index.php?m=Home&c=views&a=getEpisodesList",
+            url:"server/index.php?m=Home&c=index&a=getEpisodes",
             data:{uid:oUid}, 
             dataType:'json',
             success:function(data){  
@@ -12,7 +12,7 @@ $(document).ready(function(){
                         var str = "";  
                         $.each(data.data,function(index,array){    
                            str = '<li class="Thum">';  
-                           str = str + '<a href="./episode.php?uid='+array['uid']+'&id='+array['id']+'"><img src="'+array['pic'].substr(1)+'"/></a>';
+                           str = str + '<a href="./episode.php?uid='+array['uid']+'&id='+array['id']+'"><img src="'+array['thumbnail'].substr(1)+'"/></a>';
                            str = str + '<div class="name">'+array['title']+'</div>';
                            str = str + '</li>';
                            $("#episode_list").append(str);  
